9a20be2319677b11ccc6641a4d3da341f0bf867b
changwskr <changwskr@gmail.com>
2022-10-15 10:32:16 +0900

    환경셋팅완료

---
+++ src/main/java/com/cos/blos/BlogPjtApplication.java
@@ -0,0 +1,13 @@
+package com.cos.blos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPjtApplication.class, args);
+	}
+
+}
